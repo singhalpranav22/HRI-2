0724f7040b10fc75b576dbc8b1e26aa282e4b31d
Pranav Singhal <51447798+singhalpranav22@users.noreply.github.com>
2022-10-18 17:50:04 +0530

    Update generateRandomRobotPositions.py

--- crowd_sim/envs/generateRandomRobotPositions.py
+++ crowd_sim/envs/generateRandomRobotPositions.py
@@ -17,7 +17,7 @@ def generateRandomRobotPositions(robot_nums,robot_radius):
             xSource = round(random.uniform(-8,8), 1)
             ySource = round(random.uniform(-8,8), 1)
             while ((-8<=xSource<=-1.5 and -8<=ySource<=-1.5) or (-8<=xSource<=-1.5 and 1.5<=ySource<=8) or (1.5<=xSource<=8 and -8<=ySource<=-1.5) or (1.5<=xSource<=8 and 1.5<=ySource<=8)):
-                print(xSource,ySource)
+#                 print(xSource,ySource)
                 xSource = round(random.uniform(-8,8), 1)
                 ySource = round(random.uniform(-8,8), 1)
             xGoal = round(random.uniform(-8,8), 1)
