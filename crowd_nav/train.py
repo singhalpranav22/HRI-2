97a8ce669b7c39ba672cc2054c8118572984147b
Pranav Singhal <51447798+singhalpranav22@users.noreply.github.com>
2022-10-20 14:48:47 +0530

    Update train.py

--- crowd_nav/train.py
+++ crowd_nav/train.py
@@ -30,7 +30,7 @@ def main():
     # configure paths
     make_new_dir = True
     if os.path.exists(args.output_dir):
-        key = input('Output directory already exists! Overwrite the folder? (y/n)')
+        key = 'y'
         if key == 'y' and not args.resume:
             shutil.rmtree(args.output_dir)
         else:
